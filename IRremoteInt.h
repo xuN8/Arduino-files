cc5108f788549d8caeb867eba493a82a78c8f86c
xuN8 <32619689+xuN8@users.noreply.github.com>
2019-12-21 12:15:32 -0800

    Update IRremoteInt.h

--- IRremoteInt.h
+++ IRremoteInt.h
@@ -50,7 +50,7 @@ typedef
 		uint8_t       blinkpin;
 		uint8_t       blinkflag;       // true -> enable blinking of pin on IR processing
 		uint8_t       rawlen;          // counter of entries in rawbuf
-		unsigned int  timer;           // State timer, counts 50uS ticks.
+		unsigned int  timer0;           // State timer, counts 50uS ticks.
 		unsigned int  rawbuf[RAWBUF];  // raw data
 		uint8_t       overflow;        // Raw buffer overflow occurred
 	}
